92bae40d47592d4245527ac15b4dbb48c423cc9b
Osvaldo Bodo <informaticaobn@gmail.com>
2026-08-09 03:00:27 -0300

    24-Conexão Banco de Dados Parte-3

--- App/Models/Database/ConnectDatabase/ConnectPdoDatabase.php
+++ App/Models/Database/ConnectDatabase/ConnectPdoDatabase.php
@@ -9,16 +9,16 @@ namespace App\Models\Database\ConnectDatabase;
 
 use App\Interfaces\InterfaceConnectDatabase;
 use PDO;
+
 class ConnectPdoDatabase implements InterfaceConnectDatabase
 {
     private $pdo;
 
     public function __construct()
     {
-        $this->pdo = new \PDO("mysql:host=localhost;dbname=loja_phpoo","root", "");
+        $this->pdo = new \PDO("mysql:host=localhost;dbname=loja_phpoo","root"," ");
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     }
 
     public function connectDatabase()
