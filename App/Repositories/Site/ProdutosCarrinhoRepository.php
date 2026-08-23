6f7bc2ed5ec7933dc927fe7e453310262820b1d6
Osvaldo Bodo <informaticaobn@gmail.com>
2026-08-22 21:15:19 -0300

    47-Adicionar produto no carrinho | Parte-3

--- App/Repositories/Site/ProdutosCarrinhoRepository.php
+++ App/Repositories/Site/ProdutosCarrinhoRepository.php
@@ -44,6 +44,15 @@ class ProdutosCarrinhoRepository
 
     public function totalProdutosCarrinho()
     {
+        $total = 0;
+        foreach($this->carrinho->produtosCarrinho() as $id=>$qtd)
+        {
+            $produtoCarrinho = $this->produtoModel->find('id',$id);
+            $valorProduto = ($produtoCarrinho->produto_promocao == 1) ? $produtoCarrinho->produto_valor_promocao : $produtoCarrinho->produto_valor;
+            $total += $valorProduto * $qtd;
+
+         }
+        return $total;
 
     }
 
--- public/assets/js/site/carrinho.js
+++ public/assets/js/site/carrinho.js
@@ -4,10 +4,24 @@
 $(document).ready(function(){
     var main_content = $("#main_content");
     var center_content = main_content.find(".center_content");
-    var shopping_cart = center_content.find(".shopping_cart");
+    var shopping_cart = main_content.find(".shopping_cart");
     var products_cart = shopping_cart.find("#products_cart");
     var price_cart = shopping_cart.find(".price");
     var btn_add_carrinho = center_content.find(".btn-add-carrinho");
+
+    function totalProdutosCarrinho()
+    {
+        return $.ajax({
+            url:'/carrinho/get',
+            dataType: 'json',
+            success: function(retorno)
+            {
+                console.log(retorno);
+                products_cart.html(retorno.numeroProdutosCarrinho);
+                price_cart.html(retorno.valorProdutosCarrinho);
+            }
+        });
+    }
 
     btn_add_carrinho.on('click',function(event){
         event.preventDefault();
@@ -19,8 +33,9 @@ $(document).ready(function(){
         $.ajax({
             url:'/carrinho/add/'+idProduto,
             type:'POST',
-            success:function(retorno){
-                console.log(retorno);
+            success:function(){
+                //console.log(retorno);
+                totalProdutosCarrinho();
             }
         });
 
